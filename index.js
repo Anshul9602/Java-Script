5817bcf1917413395004520beebad536dd7f1b78
Anshul kumar <81064196+Anshul9602@users.noreply.github.com>
2022-04-15 13:05:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//   console loges
+ document.getElementsByTagName('h1')
+ console.log("hello world");
+//   alert(" Hi ")
+ document.write("this is document write");
+
+
+ //  console api
+ console.warn("this is warning");
+    console.error("this is error");  console.assert(4==(6-2)); 
+
+//    variables in js
+
+//    container to store data variables(its depeneds on data type)
+ var num1=23;
+ var num2 = 25;
+   console.log( num1 + num2);
+
+//    data type in js
+ var str1="this ansu";
+ var str2="ansu2";
+
+//  Object in js
+
+var marks ={
+    ansu:98,
+    pc:10,
+    he:45
+}
+console.log(marks);
+
+//  Boolean
+ var a= true;
+ var b= false;
+ console.log(a,b);
